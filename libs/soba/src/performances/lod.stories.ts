3cb694ac8ba3e8c04b8bbd844e33828c88026ce0
Eric Leibenguth <eric.leibenguth@gmail.com>
2026-03-16 22:43:59 +0100

    Create lod.stories.ts

---
+++ libs/soba/src/performances/lod.stories.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Meta } from '@storybook/angular';
+import { NgtArgs } from 'angular-three';
+import { NgtsOrbitControls } from 'angular-three-soba/controls';
+import { NgtsLOD } from 'angular-three-soba/performances';
+import { storyDecorators, storyFunction } from '../setup-canvas';
+
+@Component({
+	template: `
+		<ngt-group lod>
+			<ngt-mesh *lodLevel="{}">
+				<ngt-icosahedron-geometry *args="[10, 3]" />
+				<ngt-mesh-basic-material color="hotpink" wireframe />
+			</ngt-mesh>
+
+			<ngt-mesh *lodLevel="{distance: 50}">
+				<ngt-icosahedron-geometry *args="[10, 2]" />
+				<ngt-mesh-basic-material color="lightgreen" wireframe />
+			</ngt-mesh>
+
+			<ngt-mesh *lodLevel="{distance: 150, hysteresis: 0.1}">
+				<ngt-icosahedron-geometry *args="[10, 1]" />
+				<ngt-mesh-basic-material color="lightblue" wireframe />
+			</ngt-mesh>
+		</ngt-group>
+
+		<ngts-orbit-controls [options]="{ enablePan: false, enableRotate: false, zoomSpeed: 0.5 }" />
+	`,
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [NgtsLOD, NgtArgs, NgtsOrbitControls],
+})
+class DefaultLODStory {}
+
+export default {
+	title: 'Performances/LOD',
+	decorators: storyDecorators(),
+} as Meta;
+
+export const Default = storyFunction(DefaultLODStory, {
+	camera: { position: [0, 0, 100] },
+	controls: false,
+});
